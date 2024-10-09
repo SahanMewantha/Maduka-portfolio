dc458105d7fa9fd4f57d43c3f5445312926b5af0
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-09 18:53:52 +0530

    Create portfolio.js
    create portfolio page

---
+++ src/pages/portfolio.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Albem from '../components/albem_component/albem';
+import { data } from '../components/albem_component/albem_data';
+
+export default function Portfolio() {
+
+    return (
+        <>
+            <Head>
+                <title>Portfolio</title>
+                <meta name='description' content=''/>
+            </Head>
+            <main className='portfolio_page_layout'>
+                {
+                    data.map(ele => <Albem albem_data={ele}></Albem>)
+                }
+            </main>
+        </>
+    )
+}
