3e08d48a8dcb8c3c973f5282ddef9856f196b286
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-09 18:55:23 +0530

    Create albem_data.js
    albem data array

---
+++ src/components/albem_component/albem_data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        'id':1,
+        'title':'Wedding Photography',
+        'description':`Capturing the magic of your special day is my passion.
+                                From the quiet, intimate moments to the joyful celebrations,
+                                I strive to tell the unique story of your wedding through timeless,
+                                candid, and heartfelt imagery.
+                                Each photograph is a reflection of the love,
+                                emotion, and unforgettable memories shared between you and your loved ones.
+                                Let me help you preserve these precious moments so you can relive them for years to come.`,
+        'albem_image':'/image_albems/wedding/testWedding.jpg'
+    },
+    {
+        'id':2,
+        'title':'Nature Photography',
+        'description':`Through my lens,
+                                I capture the beauty and serenity of the natural world,
+                                from majestic landscapes to intimate moments in wildlife.
+                                Every photograph tells a story of the earth's raw beauty,
+                                its vibrant colors, and the peaceful connection between nature and humanity.
+                                Join me on a visual journey through forests,
+                                mountains, oceans, and the hidden wonders of the wilderness.
+                                I hope these images inspire you to explore and appreciate the world around us.`,
+        'albem_image':'/image_albems/nature/testNature.jpg'
+    }
+]
