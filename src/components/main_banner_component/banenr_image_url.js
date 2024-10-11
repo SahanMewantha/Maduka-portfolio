9f9207e88bdad0f3b60c737870e51a9a0bf37520
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-11 17:46:28 +0530

    Create banenr_image_url.js
    modify banner images data

---
+++ src/components/main_banner_component/banenr_image_url.js
@@ -0,0 +1,15 @@
+export const banner_img_url = [
+    {
+        'id':1,
+        'url':'/banner_image/wedding.jpg'
+    },
+    {
+        'id':2,
+        'url':'/banner_image/birthday.jpg'
+    }
+    ,
+    {
+        'id':3,
+        'url':'/banner_image/family.jpg'
+    }
+]
