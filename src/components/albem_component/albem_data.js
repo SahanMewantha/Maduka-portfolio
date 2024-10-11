c9e09c016508ba3a5f8bfa0a72bd92d7047e6f11
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-11 17:46:12 +0530

    Update albem_data.js
    modify albem details

--- src/components/albem_component/albem_data.js
+++ src/components/albem_component/albem_data.js
@@ -9,7 +9,8 @@ export const data = [
                                 Each photograph is a reflection of the love,
                                 emotion, and unforgettable memories shared between you and your loved ones.
                                 Let me help you preserve these precious moments so you can relive them for years to come.`,
-        'albem_image':'/image_albems/wedding/testWedding.jpg'
+        'albem_image':'/image_albems/wedding/testWedding.jpg',
+        'query':'wedding'
     },
     {
         'id':2,
@@ -22,6 +23,19 @@ export const data = [
                                 Join me on a visual journey through forests,
                                 mountains, oceans, and the hidden wonders of the wilderness.
                                 I hope these images inspire you to explore and appreciate the world around us.`,
-        'albem_image':'/image_albems/nature/testNature.jpg'
+        'albem_image':'/image_albems/nature/testNature.jpg',
+        'query':'nature'
+    },
+    {
+        'id':3,
+        'title':'Family Photography',
+        'description':`Capture the essence of your family's love and connection with our professional family photography sessions.
+                            We specialize in creating timeless portraits that reflect your unique bond and personalities.
+                            Whether it's a cozy home setting, a beautiful park,
+                            or a stunning outdoor location, we provide a relaxed and enjoyable environment for everyone
+                             Our goal is to create lasting memories that you can cherish for years to come.
+                             From candid moments to posed shots, each image tells a story of togetherness, joy, and love.`,
+        'albem_image':'/image_albems/family/testFamily.jpg',
+        'query':'family'
     }
 ]
