cb06d9618f9365d3e59496eb3ff462d5211ddd89
MininduThiranjaya <138921162+MininduThiranjaya@users.noreply.github.com>
2024-10-12 12:09:20 +0530

    Update albem_data.js
    update albem data

--- src/components/albem_component/albem_data.js
+++ src/components/albem_component/albem_data.js
@@ -14,28 +14,60 @@ export const data = [
     },
     {
         'id':2,
-        'title':'Nature Photography',
-        'description':`Through my lens,
-                                I capture the beauty and serenity of the natural world,
-                                from majestic landscapes to intimate moments in wildlife.
-                                Every photograph tells a story of the earth's raw beauty,
-                                its vibrant colors, and the peaceful connection between nature and humanity.
-                                Join me on a visual journey through forests,
-                                mountains, oceans, and the hidden wonders of the wilderness.
-                                I hope these images inspire you to explore and appreciate the world around us.`,
-        'albem_image':'/image_albems/nature/testNature.jpg',
-        'query':'nature'
+        'title':'Engagement Photography',
+        'description':`Celebrate the start of your forever with stunning engagement photos that tell your love story.
+                        I focus on capturing genuine moments of laughter, affection,
+                        and connection that reflect the uniqueness of your relationship.
+                        Whether in a cozy, intimate setting or a breathtaking outdoor location,
+                        my goal is to preserve the excitement, joy, and romance of this special chapter in your lives.
+                        Let's create timeless images that will remind you of the beautiful journey you're about to embark on—together.`,
+        'albem_image':'/image_albems/engagement/testNature.jpg',
+        'query':'engagement'
     },
     {
         'id':3,
-        'title':'Family Photography',
-        'description':`Capture the essence of your family's love and connection with our professional family photography sessions.
-                            We specialize in creating timeless portraits that reflect your unique bond and personalities.
-                            Whether it's a cozy home setting, a beautiful park,
-                            or a stunning outdoor location, we provide a relaxed and enjoyable environment for everyone
-                             Our goal is to create lasting memories that you can cherish for years to come.
-                             From candid moments to posed shots, each image tells a story of togetherness, joy, and love.`,
-        'albem_image':'/image_albems/family/testFamily.jpg',
-        'query':'family'
+        'title':'Pre-wedding Shoots',
+        'description':`Your love story deserves to be told,
+                            and a pre-wedding shoot is the perfect way to capture those special moments before you say "I do.
+                            " From candid laughter to intimate glances, we create timeless memories that reflect your unique bond.
+                            Whether you prefer a dreamy outdoor setting or a cozy indoor vibe, our goal is to freeze the beauty,
+                            warmth, and excitement of your journey together.
+                            Let us create stunning visuals that speak the language of your love.`,
+        'albem_image':'/image_albems/pre_wedding/testFamily.jpg',
+        'query':'pre-wedding'
+    },
+    {
+        'id':4,
+        'title':'After-Session Photography',
+        'description':`Capture the magic of your moments with our After-Session Photography experience!
+                        Whether it's a celebration, a milestone, or simply a day spent with loved ones,
+                        our talented photographers are dedicated to immortalizing your special occasions in stunning detail.`,
+        'albem_image':'/image_albems/after_session/testFamily.jpg',
+        'query':'after-Session'
+    },
+    {
+        'id':5,
+        'title':'Bridal Portraits',
+        'description':`Capture the essence of love and beauty with our stunning bridal portraits.
+                        Each session is designed to highlight the unique charm of the bride,
+                        showcasing her elegance and grace on her special day.
+                        Our photographers work closely with you to create a serene and joyful atmosphere, 
+                        ensuring that every emotion is beautifully reflected in your images. 
+                        From delicate close-ups to enchanting full-length shots, 
+                        we focus on the details that make you feel like the radiant centerpiece of your celebration. 
+                        Let us help you create timeless memories that you'll cherish for a lifetime.`,
+        'albem_image':'/image_albems/bride/testFamily.jpg',
+        'query':'bridal-portraits'
+    },
+    {
+        'id':6,
+        'title':'Events by Ceylon Enhance',
+        'description':`Join us for a series of exciting events that celebrate creativity, 
+                        culture, and community! From art exhibitions and music festivals to food fairs and outdoor adventures, 
+                        there's something for everyone to enjoy. Whether you're looking to immerse yourself in local traditions, 
+                        discover new talents, or simply have a great time with family and friends, 
+                        our events are designed to inspire and entertain.`,
+        'albem_image':'/image_albems/events/testFamily.jpg',
+        'query':'events'
     }
 ]
